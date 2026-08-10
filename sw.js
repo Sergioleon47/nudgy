bb375acb04f3d67187e7e36380b97115ec04896b
Sergioleon47 <sergioleon47@hotmail.com>
2026-08-09 23:48:47 -0500

    Add dark mode, aria-labels, SW cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mi-alarma-v5';
+const CACHE_NAME = 'nudgy-v6';
 const FILES_TO_CACHE = ['./', './index.html', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', (event) => {
